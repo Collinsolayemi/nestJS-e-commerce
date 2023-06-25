a3ba3336f16e60bf5dc024bebd488ee38515882b
Collinsolayemi <collinsolayemi@gmail.com>
2023-06-25 10:39:08 -0700

    user types

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,3 +1,18 @@
-import { Document } from "mongoose";
+import { Document } from 'mongoose';
 
+interface Address {
+  addr1: string;
+  addr2: string;
+  city: string;
+  state: string;
+  country: string;
+  zip: number;
+}
 
+export interface User extends Document {
+  email: string;
+  readonly password: string;
+  seller: Boolean;
+  address: Address;
+  created: Date;
+}
