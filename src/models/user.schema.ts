b857bda853a55af5064f6ce9d0a3d515a816497d
Collinsolayemi <collinsolayemi@gmail.com>
2023-06-25 10:17:10 -0700

    model schema

--- src/models/user.schema.ts
+++ src/models/user.schema.ts
@@ -1,4 +1,22 @@
-import * as mongoose from 'mongoose'
+import * as mongoose from 'mongoose';
 
-
-export const UserSchema = {}
+export const UserSchema = new mongoose.Schema({
+  name: String,
+  password: String,
+  seller: {
+    default: false,
+    type: Boolean,
+  },
+  address: {
+    addr1: String,
+    addr2: String,
+    city: String,
+    state: String,
+    country: String,
+    zip: Number,
+  },
+  created: {
+    type: Date,
+    default: Date.now,
+  },
+});
